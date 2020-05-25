db74b458e2a8eec2b1f6c1a6d0121b83ca4b2e9f
Rafe-kiho <tlsrlgh4783@yonsei.ac.kr>
2020-05-25 15:33:04 +0900

    made sign activity

---
+++ app/src/main/java/com/mediaground/yon_in/SigninActivity.java
@@ -0,0 +1,18 @@
+package com.mediaground.yon_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/mediaground/yon_in/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.mediaground.yon_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
